6a332ae7f196d3479b528799d16c43c657cc4d3f
Vladimir <88939508+xfluffymuffin@users.noreply.github.com>
2024-03-03 19:28:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,81 @@
-import pandas as pd
-from pandas import DataFrame
 from Bio import Entrez, SeqIO
+import pandas as pd
 from collections import Counter
 
-# Счетчик записей, для которых известна дата выделения
-count = 0
+
+def get_gb_records(query):
+    # Загружаем записи из GenBank по запросу из переменной 'query'
+    print('Загрузка записей...')
+    Entrez.email = 'yakrit2013@yandex.ru'
+
+    # Поиск в базе NCBI Nucleotide с учетом длины последовательности (полноты генома),
+    # сохранение результатов в record.
+    handle = Entrez.esearch(db='nucleotide',
+                            term=query,
+                            retmax=10000000)
+    record = Entrez.read(handle)
+
+    # Из record берем id результатов поиска и для каждого из них загружаем данные в формате GenBank, сохраняем в rec
+    for i in record["IdList"]:
+        download = Entrez.efetch(db="nucleotide",
+                                 id=i,
+                                 rettype="gb",
+                                 retmode="text")
+        rec = SeqIO.read(download, "genbank")
+
+        # Можно дополнительно выводить ID загружаемых записей
+        # print(rec.annotations['accessions'])
+
+        # Завершаем загрузку
+        download.close()
+
+        # Записываем все загруженное в файл output.txt. Если значение для какого-либо поля отсутствует, пишем N/A
+        outfile = open('output.txt', 'a')
+        outfile.write(f'\nGBID: {str(*rec.annotations["accessions"])}\n')
+        for key in res.keys():
+            try:
+                if key == 'GBID':
+                    res[key].append(*rec.annotations["accessions"])
+                else:
+                    res[key].append(*rec.features[0].qualifiers.get(key))
+                    outfile.write(f'{key}: {str(*rec.features[0].qualifiers.get(key)).replace(": ", ":")}\n')
+            except TypeError:
+                res[key].append('N/A')
+                outfile.write(f'{key}: N/A\n')
+        outfile.close()
+
+
+def parse():
+    # Создаем датафрейм из записей в output.txt
+    data = pd.read_csv('output.txt', sep=': ', header=None, engine='python')
+
+    # Выводим важное на экран (дату и страну сбора, виды-хозяева, штаммы, серотипы, источники выделения, информация о
+    # пациенте
+    print(f'Загружено {len((total := data[data[0] == "collection_date"][1].to_list()))} записей'
+          f'\nОбразцы выделены в следующих точках мира (чаще всего '
+          f'{(countries := Counter(data[data[0] == "country"][1].to_list()).most_common()[0])[0]}, '
+          f'{countries[1]} раз(а)):',
+          *data[data[0] == 'country'][1].dropna().unique(),
+          '\nВиды-хозяева вирусов: ', *data[data[0] == "host"][1].dropna().unique(),
+          f'\nДаты взятия образцов (известны у '
+          f'{len([x for x in total if len(str(x)) > 3])} '  # фильтр на длину даты, т.е. 'NaN' не выводятся
+          f'из {len(total)})): ',
+          *data[data[0] == "collection_date"][1].dropna().unique(),
+          '\nШтаммы: ', *data[data[0] == "strain"][1].dropna().to_list(),
+          '\nИзвестная информация о серотипах/пациентах: ', *data[data[0] == 'isolate'][1].dropna().unique(),
+          '\nИсточники выделения: ', *data[data[0] == 'isolation_source'][1].dropna().unique(),
+          '\nБолее полное описание - в файле output.txt',
+          sep='\n')
+
 
 # Словарь для упорядоченного хранения необходимых данных из записей
-res = {'country': [], 'host': [], 'collection_date': [], 'strain': [], 'isolate': []}
+res = {'GBID': [], 'country': [], 'host': [], 'collection_date': [], 'strain': [], 'isolate': [],
+       'isolation_source': []}
 
-Entrez.email = 'yakrit2013@yandex.ru'
-
-# 1) Поиск в базе NCBI Nucleotide с учетом длины последовательности (полноты генома), сохранение результатов в record.
-# Параметр retmax ограничивает максимальное количество результатов поиска
-handle = Entrez.esearch(db='nucleotide',
-                        term='"Mamastrovirus 1"[porgn] AND complete[All Fields] '
-                             'AND genome[All Fields] AND "6000"[SLEN] : "8000"[SLEN]',
-                        retmax='1000000')
-record = Entrez.read(handle)
-
-# 2) Из record берем id результатов поиска и для каждого из них загружаем данные в формате GenBank, сохраняем в rec
-for i in record["IdList"]:
-    download = Entrez.efetch(db="nucleotide", id=i, rettype="gb", retmode="text")
-    rec = SeqIO.read(download, "genbank")
-
-    # Завершаем загрузку
-    download.close()
-
-    # Здесь пригождаются названия ключей в словаре res, совпадающие с извлекаемыми из записей данными.
-    # Например, если ключ 'country', то туда записываем исключительно информацию по странам, где выделяли образцы
-    # Если в конкретной записи отсутствует необходимое значение, игнорируем ошибку (конструкция try/except)
-    for key in res.keys():
-        try:
-            res[key].append(*rec.features[0].qualifiers.get(key))
-        except TypeError:
-            pass
-
-    # 3) Создаем датафрейм с полученной из записей информацией (например, в 'Keys' будет country, а в 'Values' - China)
-    df = DataFrame({'Keys': rec.features[0].qualifiers.keys(),
-                    'Values': rec.features[0].qualifiers.values()})
-    # print(df)
-
-    # Считаем кол-во записей, для которых известна дата выделения
-    count += len(df[df['Keys'] == 'collection_date'])
-
-    # 4) Запись датафрейма в файл Excel. У меня df.to_csv игнорирует попытки передать ему какой-либо sep, и
-    # в итоге в Excel наблюдается каша в одном столбце. С df.to_excel тоже есть проблема: if_sheet_exists='overlay'
-    # не дает дополнять датафреймы на одном листе (получается информация только для одной записи,
-    # т.е. все перезаписывается). Приходится добавлять каждый датафрейм на новый лист
-    with pd.ExcelWriter('output.xlsx',
-                        mode='a', if_sheet_exists='new') as writer:
-        df.to_excel(writer, sheet_name="Main", index=False)
-
-    # 5) В поле 'strain' почему-то может попадать информация как о серотипе, так и о пациенте
-print(f'\nОбразцы выделены в следующих точках мира, чаще всего {Counter(res["country"]).most_common()[0]}:',
-      *res["country"],
-      '\nВиды-хозяева вирусов: ', *res['host'],
-      f'\nДаты взятия образцов (известны у {count} из {len(record["IdList"])}): ', *res['collection_date'],
-      '\nШтаммы: ', *res['strain'],
-      '\nИнформация о серотипе или пациенте (?): ', *res['isolate'], sep='\n')
+# Если файла output.txt не существует, то и записи отсутствуют. Тогда сначала скачиваем их. В противном случае работаем
+# с готовым файлом
+try:
+    parse()
+except FileNotFoundError:
+    get_gb_records('"Mamastrovirus 1"[porgn] AND "6000"[SLEN] : "8000"[SLEN]')
+    parse()
