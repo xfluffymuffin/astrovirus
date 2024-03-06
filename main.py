84422d79c91c08e96d4b8fb9e5907805e619fe29
Vladimir <88939508+xfluffymuffin@users.noreply.github.com>
2024-03-06 11:43:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from Bio import Entrez, SeqIO
 import pandas as pd
 from collections import Counter
 
-
+# Функция для скачивания и сохранения полных GenBank-записей в файл records.gb
+# и частичной записи данных в словарь res
 def get_gb_records(query):
     # Загружаем записи из GenBank по запросу из переменной 'query'
     print('Загрузка записей...')
@@ -15,67 +16,62 @@ def get_gb_records(query):
                             retmax=10000000)
     record = Entrez.read(handle)
 
-    # Из record берем id результатов поиска и для каждого из них загружаем данные в формате GenBank, сохраняем в rec
+    # Из record берем id результатов поиска и для каждого из них загружаем данные в формате GenBank, сохраняем в rec.
     for i in record["IdList"]:
-        download = Entrez.efetch(db="nucleotide",
-                                 id=i,
-                                 rettype="gb",
-                                 retmode="text")
-        rec = SeqIO.read(download, "genbank")
+        download_for_writing = Entrez.efetch(db="nucleotide",
+                                             id=i,
+                                             rettype="gb",
+                                             retmode="text")
+	# Создаем выходной файл с записями GenBank – records.gb
+        with open("records.gb", "a") as outfile:
+            outfile.write(download_for_writing.read())
 
-        # Можно дополнительно выводить ID загружаемых записей
-        # print(rec.annotations['accessions'])
-
-        # Завершаем загрузку
-        download.close()
-
-        # Записываем все загруженное в файл output.txt. Если значение для какого-либо поля отсутствует, пишем N/A
-        outfile = open('output.txt', 'a')
-        outfile.write(f'\nGBID: {str(*rec.annotations["accessions"])}\n')
+        download_for_parsing = Entrez.efetch(db="nucleotide",
+                                             id=i,
+                                             rettype="gb",
+                                             retmode="text")
+        
+        # Загруженные данные также частично сохраняем в словарь res.
+        # Если информация по какому-либо квалификатору отсутствует, пишем NaN
+        rec = SeqIO.read(download_for_parsing, "genbank")
         for key in res.keys():
             try:
                 if key == 'GBID':
                     res[key].append(*rec.annotations["accessions"])
                 else:
                     res[key].append(*rec.features[0].qualifiers.get(key))
-                    outfile.write(f'{key}: {str(*rec.features[0].qualifiers.get(key)).replace(": ", ":")}\n')
             except TypeError:
-                res[key].append('N/A')
-                outfile.write(f'{key}: N/A\n')
-        outfile.close()
+                res[key].append('NaN')
 
-
-def parse():
-    # Создаем датафрейм из записей в output.txt
-    data = pd.read_csv('output.txt', sep=': ', header=None, engine='python')
-
-    # Выводим важное на экран (дату и страну сбора, виды-хозяева, штаммы, серотипы, источники выделения, информация о
-    # пациенте
-    print(f'Загружено {len((total := data[data[0] == "collection_date"][1].to_list()))} записей'
-          f'\nОбразцы выделены в следующих точках мира (чаще всего '
-          f'{(countries := Counter(data[data[0] == "country"][1].to_list()).most_common()[0])[0]}, '
-          f'{countries[1]} раз(а)):',
-          *data[data[0] == 'country'][1].dropna().unique(),
-          '\nВиды-хозяева вирусов: ', *data[data[0] == "host"][1].dropna().unique(),
-          f'\nДаты взятия образцов (известны у '
-          f'{len([x for x in total if len(str(x)) > 3])} '  # фильтр на длину даты, т.е. 'NaN' не выводятся
-          f'из {len(total)})): ',
-          *data[data[0] == "collection_date"][1].dropna().unique(),
-          '\nШтаммы: ', *data[data[0] == "strain"][1].dropna().to_list(),
-          '\nИзвестная информация о серотипах/пациентах: ', *data[data[0] == 'isolate'][1].dropna().unique(),
-          '\nИсточники выделения: ', *data[data[0] == 'isolation_source'][1].dropna().unique(),
-          '\nБолее полное описание - в файле output.txt',
-          sep='\n')
-
+# Функция для подсчета и вывода встречаемости каждого квалификатора
+def count_occurences(qualifier):
+    return [x[0] + ': ' + str(x[1]) for x in dict(Counter(res[qualifier])).items() if x[0] != 'NaN']
 
 # Словарь для упорядоченного хранения необходимых данных из записей
 res = {'GBID': [], 'country': [], 'host': [], 'collection_date': [], 'strain': [], 'isolate': [],
        'isolation_source': []}
 
-# Если файла output.txt не существует, то и записи отсутствуют. Тогда сначала скачиваем их. В противном случае работаем
-# с готовым файлом
-try:
-    parse()
-except FileNotFoundError:
-    get_gb_records('"Mamastrovirus 1"[porgn] AND "6000"[SLEN] : "8000"[SLEN]')
-    parse()
+get_gb_records('"Mamastrovirus 1"[porgn] AND "6000"[SLEN] : "8000"[SLEN]')
+
+# Сохраняем данные из словаря res в датафрейм pandas и файл metadata.csv
+data = pd.DataFrame.from_dict(res)
+data.to_csv('metadata.csv', sep=',', index=False)
+
+# Выводим часть данных в файл main_output.txt
+with open('main_output.txt', 'w') as outfile:
+    print(f'Загружено {data.GBID.count()} записей',
+      f'\nОбразцы выделены в следующих точках мира (чаще всего '
+      f'{(count := Counter(res["country"]).most_common()[0])[0]}: {count[1]} раз(а)):',
+      *count_occurences('country'),
+      '\nВиды-хозяева вирусов: ',  *count_occurences('host'),
+      f'\nДаты взятия образцов (известны у '
+      f'{data.collection_date.count() - data.collection_date.str.contains("N/A").sum()} '
+      f'из {data.collection_date.count()})): ',
+      *count_occurences('collection_date'),
+      '\nШтаммы: ', *count_occurences('strain'),
+      '\nИзвестная информация о серотипах/пациентах: ',
+      *count_occurences('isolate'),
+      '\nИсточники выделения: ',
+      *count_occurences('isolation_source'),
+      '\nБолее полное описание - в файле output.gb',
+      sep='\n', file=outfile)
