3c9f79ebe0fcecd6a8755081ef05ef29b700c519
Vladimir <88939508+xfluffymuffin@users.noreply.github.com>
2024-04-14 23:51:16 +0300

    Update clean_records.py

--- Task2/clean_records.py
+++ Task2/clean_records.py
@@ -14,6 +14,7 @@ def clean_records(fasta_file):
                 file.write(">" + str(i.id) + "\n" +
                            str(i.seq.upper()) + "\n")
 
+
 # Переписываем fasta-файл так, чтобы в каждой строке с последовательностью содержалось 60 символов
 def fix_format(fasta):
     SeqIO.write(SeqIO.parse(fasta, "fasta"),
@@ -21,20 +22,26 @@ def fix_format(fasta):
                 'fasta')
     os.remove("D:\PyCharm\BioPython\intermediate.fasta")
 
+
 # Проверяем, есть ли в заголовках оставшихся записей дата взятия образца
 def check_collection_date(fasta):
     with open(fasta, 'r') as file:
         for line in file:
             for i in range(1900, 2025):
                 if ">" in line:
-                    no_coll_date.append(line.strip(">\n"))
+                    all_rec.append(line.strip(">\n"))
+
+                if ">" in line and str(i) in line:
+                    coll_date.append(line.strip(">\n"))
+
     with open("records_no_collection_date.txt", 'w') as writer:
         # Если даты нет, записываем весь заголовок в отдельный файл для дальнейшей работы
-        for fasta_header in list(set(no_coll_date)): # Превращение в set(no_coll_date) и обратно - для отсеивания повторяющихся значений списка
+        for fasta_header in list(set([x for x in all_rec if
+                                      x not in coll_date])):  # Превращение в set(no_coll_date) и обратно - для отсеивания повторяющихся значений списка
             writer.write(fasta_header + "\n")
 
 
-seqs, no_coll_date = [], []
+seqs, coll_date, all_rec = [], [], []
 clean_records("D:\PyCharm\BioPython\Mamastrovirus_1_complete_genome_records.fasta")
 fix_format("D:\PyCharm\BioPython\intermediate.fasta")
 check_collection_date("D:\PyCharm\BioPython\Mamastrovirus_1_complete_genome_records_CLEARED.fasta")
