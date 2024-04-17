b42b9ee6041c25488e04f1099ab58b10b9a1b960
Vladimir <88939508+xfluffymuffin@users.noreply.github.com>
2024-04-17 12:18:59 +0300

    Create clean_records_fixed.py

---
+++ Task2/clean_records_fixed.py
@@ -0,0 +1,35 @@
+from Bio import SeqIO
+import os
+
+
+# Функция пробегает по записям fasta-файла и сначала формирует из них список, а затем достает только те, которые
+# не содержат слова "sewage", т.е. убираем образцы, взятые из сточных вод
+def clean_records(fasta_file):
+    seqs_cleaned = []
+    for rec_num, seq in enumerate(SeqIO.parse(fasta_file, "fasta")): # ЮА: номер записи называть record никто не запрещает, но это путает, так как обычно так называют объекты SeqRecord
+        seqs_cleaned.append(seq)
+
+
+# Проверяем, есть ли в заголовках оставшихся записей дата взятия образца
+def check_collection_date(fasta):
+    with open(fasta, 'r') as file:
+        for line in file:
+            for i in range(1900, 2025):
+                if ">" in line:
+                    # Все заголовки
+                    all_rec.append(line.strip(">\n"))
+
+                if ">" in line and str(i) in line:
+                    # Заголовки с датами
+                    coll_date.append(line.strip(">\n"))
+
+    with open("records_no_collection_date.txt", 'w') as writer:
+        # Если даты нет, записываем весь заголовок в отдельный файл для дальнейшей работы
+        for fasta_header in list(set([x for x in all_rec if
+                                      x not in coll_date])):  # Превращение в set(no_coll_date) и обратно - для отсеивания повторяющихся значений списка
+            writer.write(fasta_header + "\n")
+
+
+coll_date, all_rec = [], []
+clean_records("Mamastrovirus_1_complete_genome_records.fasta")
+check_collection_date("ALT_Mamastrovirus_1_complete_genome_records_CLEARED.fasta")
