4d15f94e3623223377b6fe6d9675b6c89137e6b5
Vladimir <88939508+xfluffymuffin@users.noreply.github.com>
2024-06-03 21:59:56 +0300

    Update metadata_editor.py

--- Task2/add_serotypes_data/metadata_editor.py
+++ Task2/add_serotypes_data/metadata_editor.py
@@ -1,49 +1,61 @@
-def data_editor(path_old, path_new):
-    mod = []
-    ser = {"1": ["FJ755404", "KF211475", "AY720892", "JN887820", "FJ755403", "FJ755402", "FJ755405", "KC342249",
-                 "ON571594", "NC_030922", "OQ630461", "LC694996", "LC694997", "MW485040", "MW485039", "MW485038",
-                 "MW485041",
-                 "L23513", "MK059949", "Z25771", "HQ398856", "MT832896", "MT832892", "MT832895", "MT832894", "MT832893",
-                 "MT832897", "MN433703", "MN433704", "LC694990", "LC694987", "LC694992", "LC694993", "LC694994",
-                 "LC694986", "LC694989",
-                 "LC694995", "ON571596", "ON571597", "ON571597", "LC694988", "LC694991"],
-           "2": ["KF039910", "MK059950", "MN433705", "MW485042", "KF039911", "KC285152"],
-           "3": ["MT267483", "MK059951", "GU223905", "GU732187", "JF491430", "MW485043", "MZ603079", "MG571777",
-                 "MK296753", "LC732124", "MN444721", "LC694985", "LC732122", "LC732123", "OP432297", "OP432300",
-                 "OP432298", "OP432294", "OP432296", "OP432301"],
-           "4": ["DQ070852", "AY720891", "MK059952", "KC285113", "KF039912", "MT906853", "MT906854", "MT906855",
-                 "KF039913", "DQ344027", "OP432293", "OP432299", "OR371570", "OP432295", "ON571598"],
-           "5": ["DQ028633", "MK059953", "JQ403108", "MT267476", "MN433706", "MW485044", "MW485045", "MF684776",
-                 "ON571595", "OR222910", "MT906858", "MT906859", "MT906856", "MT906857"],
-           "6": ["HM237363", "MK059954", "GQ495608", "GQ901902"], "8": ["KP862744", "AF260508", "MK059956"]}
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+
+def edit_metadata(path_old, path_new):
+    # Файл с аннотацией
+    ser = pd.read_csv(r"D:\PyCharm\BioPython\tree_DNA_alignment\iTOL\ORF2_DNA_tree_ann.txt",
+                      sep="\t",
+                      header=None,
+                      skiprows=3) \
+          .drop(columns=[1, 3, 4]) \
+          .rename({0: 'leaf_name', 2: 'color'}, axis=1) \
+          .head(104)
+    # Первые строки с названиями записей содержат оригинальные названия (все остальное - сочетания как в дереве)
+
+    # Посмотреть уникальные значения RGB цветов
+    # print(ser.color.unique())
+
+    conditions = [ser['color'] == 'rgb(191, 144, 0)',  # yellow
+                  ser['color'] == 'rgb(116, 27, 71)',  # magenta
+                  ser['color'] == 'rgb(0, 211, 32)',  # green
+                  ser['color'] == 'rgb(204, 0, 0)',  # red
+                  ser['color'] == 'rgb(255, 126, 193)',  # pink
+                  ser['color'] == 'rgb(0, 0, 255)',  # blue
+                  ser['color'] == 'rgb(111, 168, 220)',  # light blue
+                  ser['color'] == 'rgb(180, 95, 6)']  # brown
+
+    choices = ['serotype_8', 'serotype_4', 'serotype_6', 'serotype_5',
+               'serotype_2', 'serotype_7', 'serotype_3', 'serotype_1']  # same order
+
+    ser['serotype'] = np.select(conditions, choices)
+    ser['full_name'] = ser['leaf_name'] + "_" + ser['serotype']
+
+    mod = [str(x[0]) + '_serotype_' + str(x[-1]) for x in list(ser.full_name.str.split('_'))]
+    # print(mod)
+
+    res_lines = []
 
     with open(path_old, "r") as old:
         lines = old.read().splitlines()
 
         for line in lines:
             if "GBID" in line:
-                mod.append(line + ",serotype")
-            elif "MK059955" in line:
-                mod.append(line + ",serotype_7")
+                res_lines.append(line + ",serotype")
             else:
-                for key, values in ser.items():
-                    for id in values:
-                        if key == "1" and id in line:
-                            mod.append(line + ",serotype_1")
-                        if key == "2" and id in line:
-                            mod.append(line + ",serotype_2")
-                        if key == "3" and id in line:
-                            mod.append(line + ",serotype_3")
-                        if key == "4" and id in line:
-                            mod.append(line + ",serotype_4")
-                        if key == "5" and id in line:
-                            mod.append(line + ",serotype_5")
-                        if key == "6" and id in line:
-                            mod.append(line + ",serotype_6")
-                        if key == "8" and id in line:
-                            mod.append(line + ",serotype_8")
+                for x in mod:
+                    for i in range(9):
+                        if x.endswith(f"_{i}") and x.split('_')[0] in line:
+                            res_lines.append(line + f",serotype_{i}")
+
     with open(path_new, "w") as new:
-        new.write("\n".join([x for x in mod]))
+        new.write("\n".join([x for x in res_lines]))
 
 
-data_editor(r"D:\PyCharm\BioPython\add_serotypes_data\metadata.csv", r"D:\PyCharm\BioPython\add_serotypes_data\metadata_serotypes_colored.csv")
+edit_metadata(r"D:\PyCharm\BioPython\add_serotypes_data\metadata.csv",
+              r"D:\PyCharm\BioPython\add_serotypes_data\metadata_serotypes_fixed.csv")
