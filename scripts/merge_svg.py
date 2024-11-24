36b64ab88be686502030e55aa303b526fa1f8813
Vladimir <88939508+xfluffymuffin@users.noreply.github.com>
2024-11-24 17:25:32 +0300

    Update merge_svg.py

--- scripts/merge_svg.py
+++ scripts/merge_svg.py
@@ -3,7 +3,7 @@ import os.path
 
 svg_files = []
 
-for dirpath, dirnames, filenames in os.walk(r"path_to_directory"):
+for dirpath, dirnames, filenames in os.walk(r"path_to_directory"): # Директория с объединяемыми svg-изображениями
     for filename in [f for f in filenames if f.endswith(".svg")]:
         svg_files.append(os.path.join(dirpath, filename))
         # print(os.path.join(dirpath, filename))
@@ -19,13 +19,13 @@ layout = sc.Figure(
     sc.Panel(sc.SVG(svg_files[0]).move(0, 0)),
     sc.Panel(sc.SVG(svg_files[1]).move(420, 0)),
 
-    sc.Panel(sc.SVG(svg_files[2]).move( 0, 340)),
-    sc.Panel(sc.SVG(svg_files[3]).move( 420, 340)),
+    sc.Panel(sc.SVG(svg_files[2]).move(0, 340)),
+    sc.Panel(sc.SVG(svg_files[3]).move(420, 340)),
 
-    sc.Panel(sc.SVG(svg_files[4]).move( 0, 2 * 340)),
-    sc.Panel(sc.SVG(svg_files[5]).move( 420, 2 * 340)),
+    sc.Panel(sc.SVG(svg_files[4]).move(0, 2 * 340)),
+    sc.Panel(sc.SVG(svg_files[5]).move(420, 2 * 340)),
 
-    sc.Panel(sc.SVG(svg_files[6]).move( 210, 3 * 340)))
+    sc.Panel(sc.SVG(svg_files[6]).move(210, 3 * 340)))
 
     #sc.Panel(sc.SVG(svg_files[6]).move(0, 3 * 340)),
     #sc.Panel(sc.SVG(svg_files[7]).move(420, 3 * 340)))
